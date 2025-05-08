9e61e23deec010ef233282250c6991a663cad2b5
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-05-08 23:35:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const selectColor=document.querySelector("#colorSelect");
+document.querySelector('input[type="button"]').addEventListener('click',()=>{
+	const selectOption=selectColor.options[selectColor.selectedIndex];
+	selectColor.removeChild(selectOption);
+});
